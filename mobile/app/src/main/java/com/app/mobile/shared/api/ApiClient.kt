39490c047f62a09996a4e0f05fa5dc902337adc5
JasCiely <168708885+JasCiely@users.noreply.github.com>
2026-06-04 23:09:54 +0800

    feat: final commit

--- mobile/app/src/main/java/com/app/mobile/shared/api/ApiClient.kt
+++ mobile/app/src/main/java/com/app/mobile/shared/api/ApiClient.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object ApiClient {
 
-    const val BASE_URL = "http://192.168.1.7:8080"
+    const val BASE_URL = "http://192.168.1.8:8080"
 
     val http: OkHttpClient by lazy {
         OkHttpClient.Builder()
